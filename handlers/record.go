9edcb4f29f47d1603014ac2343c8a7bc6ed86bd5
sunliang <sunliang711@163.com>
2020-12-23 15:42:12 +0800

    log msg;

--- handlers/record.go
+++ handlers/record.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -21,6 +22,8 @@ type UpdateRecordReq struct {
 
 // UpdateRecord 根据请求来更新主机记录，如果不存在，则会新增主机记录
 func UpdateRecord(c *gin.Context) {
+	remoteAddr := c.Request.RemoteAddr
+	logrus.Infof("Remote address: %v",remoteAddr)
 	var req UpdateRecordReq
 	err := c.BindJSON(&req)
 	if err != nil {
@@ -36,13 +39,19 @@ func UpdateRecord(c *gin.Context) {
 	}
 	regionID := viper.GetString("server.regionID")
 	recordOperator, err := recordOperation.NewOperator(regionID, req.AccessKey, req.AccessSecret, "", "")
+	if err != nil {
+		logrus.Infof("NewOperator error: %v",err)
+		c.JSON(200,gin.H{"message":fmt.Sprintf("Internal error: %v",err)})
+		return
+	}
 	err = recordOperator.DoUpdate(req.NewIP, req.RR, req.Domain, req.Type, req.TTL)
 
 	if err != nil {
-		logrus.Info(err)
-		c.JSON(200, gin.H{"message": err})
+		msg := fmt.Sprintf("update error: %v",err)
+		logrus.Info(msg)
+		c.JSON(200, gin.H{"message": msg})
 	} else {
-		logrus.Info("OK")
-		c.JSON(200, gin.H{"message": "OK"})
+		logrus.Info("updated")
+		c.JSON(200, gin.H{"message": "updated"})
 	}
 }
