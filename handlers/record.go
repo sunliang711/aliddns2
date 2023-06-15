03a728a8849927137d90d3594891c22eaa24c331
sunliang711 <sunliang711@163.com>
2023-06-15 15:33:36 +0800

    access secret

--- handlers/record.go
+++ handlers/record.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
@@ -21,6 +22,68 @@ type UpdateRecordReq struct {
 	AccessSecret string `json:"access_secret"`
 }
 
+func (req *UpdateRecordReq) check(ctx *gin.Context) error {
+	var err error
+	if req.NewIP == "" {
+		logrus.Infof("use client ip instead of request body")
+		req.NewIP = ctx.ClientIP()
+	}
+
+	if req.RR == "" {
+		return fmt.Errorf("require rr")
+	}
+
+	if req.Domain == "" {
+		return fmt.Errorf("require domain")
+	}
+
+	if req.AccessKey == "" || req.AccessSecret == "" {
+		req.AccessKey, req.AccessSecret, err = getAccessKey(req.Domain)
+		if err != nil {
+			return err
+		}
+	}
+
+	if req.TTL == "" {
+		req.TTL = "600"
+	}
+
+	if req.Type == "" {
+		req.Type = "A"
+	}
+
+	return nil
+}
+
+func getAccessKey(domain string) (string, string, error) {
+	accessKeys := strings.Split(viper.GetString("server.accessKeys"), ";")
+	// accessKey format: <domain>:<access_key>:<access_secret>
+	for _, accessKey := range accessKeys {
+		parts := strings.Split(accessKey, ":")
+		if len(parts) != 3 {
+			logrus.Warnf("invalid format or accessKeys in config")
+			break
+		}
+		dm := parts[0]
+		key := parts[1]
+		secret := parts[2]
+		if dm == domain {
+			logrus.Infof("got access key for domain: %s", domain)
+			return key, secret, nil
+		}
+	}
+
+	defaultAccessKey := viper.GetString("server.defaultAccessKey")
+	defaultAccessSecret := viper.GetString("server.defaultAccessSecret")
+	if defaultAccessKey == "" || defaultAccessSecret == "" {
+		return "", "", fmt.Errorf("no default access key or secret")
+	}
+
+	logrus.Infof("use default access key and secret")
+	// return defaultAccessKey defaultAccessSecret
+	return defaultAccessKey, defaultAccessSecret, nil
+}
+
 // UpdateRecord 根据请求来更新主机记录，如果不存在，则会新增主机记录
 func UpdateRecord(c *gin.Context) {
 	remoteAddr := c.Request.RemoteAddr
@@ -32,47 +95,20 @@ func UpdateRecord(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "bad request,invalid json"})
 		return
 	}
-	newIP := req.NewIP
-	if newIP == "" {
-		logrus.Infof("use client ip instead of request body")
-		newIP = c.ClientIP()
-	}
 
-	accessKey := req.AccessKey
-	if accessKey == "" {
-		logrus.Infof("use default access key")
-		accessKey = viper.GetString("server.defaultAccessKey")
-	}
-	accessSecret := req.AccessSecret
-	if accessSecret == "" {
-		logrus.Infof("use default access secret")
-		accessSecret = viper.GetString("server.defaultAccessSecret")
-	}
-
-	rr := req.RR
-	domain := req.Domain
-	domainType := req.Type
-	if domainType == "" {
-		domainType = "A"
-	}
-	ttl := req.TTL
-	if ttl == "" {
-		ttl = "600"
-	}
-
-	if newIP == "" || rr == "" || domain == "" || domainType == "" || accessKey == "" || accessSecret == "" {
-		logrus.Warnf("some request field is empty")
-		c.JSON(200, gin.H{"message": types.ErrReqFieldEmpty, "format": "need new_ip(optional),rr,domain,type,ttl,access_key(use default when empty),access_secret(use default when empty)"})
+	if err = req.check(c); err != nil {
+		logrus.Errorf("%s", err.Error())
+		c.JSON(200, gin.H{"message": err.Error()})
 		return
 	}
 	regionID := viper.GetString("server.regionID")
-	recordOperator, err := recordOperation.NewOperator(regionID, accessKey, accessSecret, "", "")
+	recordOperator, err := recordOperation.NewOperator(regionID, req.AccessKey, req.AccessSecret, "", "")
 	if err != nil {
 		logrus.Infof("NewOperator error: %v", err)
 		c.JSON(200, gin.H{"message": fmt.Sprintf("Internal error: %v", err)})
 		return
 	}
-	err = recordOperator.DoUpdate(newIP, rr, domain, domainType, ttl)
+	err = recordOperator.DoUpdate(req.NewIP, req.RR, req.Domain, req.Type, req.TTL)
 
 	if err == types.ErrNotNeedUpdate {
 		msg := fmt.Sprintf("%v", err)
@@ -83,7 +119,7 @@ func UpdateRecord(c *gin.Context) {
 		logrus.Info(msg)
 		c.JSON(200, gin.H{"code": 1, "message": msg})
 	} else {
-		msg := fmt.Sprintf("Update %v.%v to %v", rr, domain, newIP)
+		msg := fmt.Sprintf("Update %v.%v to %v", req.RR, req.Domain, req.NewIP)
 		logrus.Info(msg)
 		c.JSON(200, gin.H{"code": 0, "message": msg})
 	}
